f623ed6dfeb29bd54fcc717b1461e1bfb4272f9a
ati-tech <a.trading.invest@gmail.com>
2025-02-13 04:53:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Whenever someone visits '/', we redirect to Google
 app.get('/', (req, res) => {
-  res.redirect('https://www.google.com');
+  res.redirect('https://embeds.beehiiv.com/8e14a814-c5fb-4b5f-a671-86dd4ae6795c');
 });
 
 // Let's use the port Railway provides, or default to 3000
